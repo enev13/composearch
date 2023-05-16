5d6f4d46a5a5fb71b6a8130164a43b736db09151
enev13 <enev@engineer.bg>
2023-05-16 16:54:11 +0300

    migrate to MongoDB Atlas

--- composearch/settings.py
+++ composearch/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-xd(45loqojkk==4sijcu!tif#u*a!h=43i2f!ebmd#_zxb(e)f"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -76,9 +77,17 @@ WSGI_APPLICATION = "composearch.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "ENGINE": "djongo",
+        "NAME": "composearch",
+                "ENFORCE_SCHEMA": False,
+                "CLIENT": {
+                    "host": config("MONGO_HOST"),
+                }
+    },
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
 }
 
 
