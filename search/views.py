551fa549ffb095e83f0342c4fab8c429d80b39a9
enev13 <enev@engineer.bg>
2023-06-10 11:15:47 +0300

    log search time

--- search/views.py
+++ search/views.py
@@ -1,6 +1,11 @@
+import logging
+import time
+
 from django.shortcuts import render
 
 from search.search import perform_search
+
+log = logging.getLogger(__name__)
 
 
 async def search_async(query, loop):
@@ -12,10 +17,12 @@ def home_view(request):
 
 
 async def results_view(request):
+    start_time = time.time()
     query = request.GET.get("query")
     if query:
         results = await perform_search(query)
     else:
         results = []
-
+    end_time = time.time()
+    log.debug(f"Search took {end_time - start_time} seconds")
     return render(request, "search/results.html", {"results": results, "query": query})
