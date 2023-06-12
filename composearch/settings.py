49e5ff942257105737db667249e574d28b1a4368
enev13 <enev@engineer.bg>
2023-06-12 14:37:51 +0300

    add CSRF_TRUSTED_ORIGINS

--- composearch/settings.py
+++ composearch/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=False, cast=bool)
 PRODUCTION = config("PRODUCTION", default=False, cast=bool)
 ALLOWED_HOSTS = [config("ALLOWED_HOSTS")]
-
+CSRF_TRUSTED_ORIGINS = [config("ALLOWED_HOSTS")]
 
 # Application definition
 
