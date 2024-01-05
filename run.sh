24ac0fa754a934f9292d633cc8084099cba851c8
enev13 <enev@engineer.bg>
2024-01-05 16:24:12 +0200

    Update gunicorn bind port to 80

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ python3 manage.py collectstatic --noinput
 python3 manage.py migrate
 python3 manage.py flush --no-input
 python3 manage.py loaddata distributors.json
-gunicorn --bind 0.0.0.0:8000 composearch.wsgi --timeout 60 --workers 3
+gunicorn --bind 0.0.0.0:80 composearch.wsgi --timeout 60 --workers 3
